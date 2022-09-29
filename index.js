6bcd09879ac9ed6cd97c7edb130e730e835c6f18
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-09-29 09:55:26 +0300

    FG

--- index.js
+++ index.js
@@ -3,3 +3,38 @@ const c = canvas.getContext('2d');
 
 canvas.width = 1024
 canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite{
+    constructor(position, velocity){
+        this.position = position
+        this.velocity = velocity
+    }
+
+    draw(){
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x,this.position.y, 50, 150)
+    }
+}
+
+const player = new Sprite({
+    x: 0,
+    y: 0
+})
+
+player.draw();
+
+const enemy = new Sprite({
+    x: 400,
+    y: 0
+})
+
+enemy.draw();
+
+function animate(){
+    window.requestAnimationFrame(animate)
+}
+
+animate()
+

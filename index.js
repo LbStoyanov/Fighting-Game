88665f84e760524ce34da51c5a39535346b6a0f0
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-09-30 11:06:07 +0300

    FG

--- index.js
+++ index.js
@@ -133,17 +133,33 @@ function rectangularCollision({rectangle1, rectangle2}) {
   );
 }
 
-let timer = 12;
+function determineWinner({player,enemy,timerId}){
+    clearTimeout(timerId);
+    document.querySelector('#displayText').style.display = 'flex';
+
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie';
+    } else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins!';
+    }else {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins!';
+    }
+}
+
+let timer = 60;
+let timerId;
+
 function decreaseTimer(){
-    setTimeout(decreaseTimer,1000);
     if (timer > 0) {
+        timerId = setTimeout(decreaseTimer,1000);
         timer--;
         document.querySelector('#timer').innerHTML = timer;
     }
 
-    if (player.health === enemy.health) {
-        
+    if (timer === 0) {
+        determineWinner({player,enemy,timerId});
     }
+    
 }
 
 decreaseTimer();
@@ -195,6 +211,12 @@ function animate() {
     player.health -= 20;
     document.getElementById('playerHealth').style.width = player.health + '%';
   }
+
+  // End game based on opponents health
+  if (enemy.health === 0 || player.health === 0) {
+    determineWinner({player,enemy,timerId});
+  }
+
 }
 
 animate();
